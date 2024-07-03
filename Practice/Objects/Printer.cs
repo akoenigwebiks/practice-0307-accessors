c0010069f4b33317bfabfc060a06e30c69373a51
Avi E Koenig <darkmift@gmail.com>
2024-07-03 14:13:10 +0300

    Add project files.

---
+++ Practice/Objects/Printer.cs
@@ -0,0 +1,68 @@
+﻿namespace Practice.Objects
+{
+    public class Printer
+    {
+
+        public string Value { get; set; }
+        private int _number;
+        public int Number
+        {
+            get { return _number; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _number = value;
+                }
+            }
+        }
+        public Printer(string value, int num)
+        {
+            Value = value;
+            Number = num;
+        }
+        public Printer(int num)
+        {
+            Value = "";
+            Number = num;
+        }
+        public void Print()
+        {
+            Console.WriteLine(Value);
+            Console.WriteLine(Number);
+        }
+        /*
+
+        //Printer: private int _number
+        //Printer: public int Number { get { return _number } set { if (value) > 0 _number = value }
+
+        //Program -> Main: Printer printer = new Printer(3);  printer.Number = 78;  printer.Number = -8;
+
+        private string _value;
+        public string Value
+        {
+            get { return _value; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _value = value;
+                }
+            }
+        }
+
+        public Printer(string value, int number)
+        {
+            // _number = number
+            _value = value;
+
+            Value = "Sdf";
+   
+        }
+        public void Print()
+        {
+            Console.WriteLine(Value);
+        }
+        */
+    }
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using Practice.Objects;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Printer printer = new Printer("hello",3);
+            //Console.WriteLine(printer.Value);
+            //printer.Value = "Isaschar";
+            //Console.WriteLine(printer.Value);  
+            Printer printer2 = new Printer(3);
+            printer2.Number = -22;
+            Console.WriteLine(printer2.Number);
+            Console.WriteLine("----------------");
+            printer2.Number = 42;
+            Console.WriteLine(printer2.Number);
+            Console.WriteLine("----------------");
+
+        }
+    }
+}
